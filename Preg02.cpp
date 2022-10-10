7c331e0aa6aca32c1128c122aab97ce5f24f6284
Camarena <20a3050324@untels.edu.pe>
2022-10-10 11:29:51 -0500

    Pregunta02 - Listo

---
+++ Preg02.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+
+int main(){
+	
+	int x, suma = 0;
+    cout<<"Ingrese el numero de elementos de la sumatoria:\n";
+    cin>>x;
+    for (int i=1; i<=x;i++){
+      suma += i;
+   }
+	cout<<"\nLa suma de los "<<x<<" primeros numeros naturales es "<<suma;
+       
+	return 0;
+}
